f5773df7b325d2b8eda379d6258affef6e28fb39
hyousen <hsakuchi1209@gmail.com>
2024-11-30 18:56:16 +0900

    first commit

---
+++ src/main/java/com/hsakuchi/hobby/ProgressLogApplication.java
@@ -0,0 +1,13 @@
+package com.hsakuchi.hobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsakuchi/hobby/ProgressLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsakuchi.hobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
