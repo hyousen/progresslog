fbb4da114891b1278fc9754b85867e97cbed01dc
hyousen <hsakuchi1209@gmail.com>
2024-12-22 16:21:00 +0900

    タイトルや大まかな部分ができた

--- src/main/java/com/hsakuchi/hobby/TextFileWrite.java
+++ src/main/java/com/hsakuchi/hobby/TextFileWrite.java
@@ -3,14 +3,16 @@ package com.hsakuchi.hobby;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TextFileWrite {
 
-	public void textWrite (String sentence, String fileName) {
+	public void textWrite(String sentence, String fileName) {
 
 		try {
 			File file = new File("C:\\hsakuchi\\work\\ProgressLog\\src\\main\\resources\\templates\\" + fileName + ".txt");
-
+			
 			if (checkBeforeWritefile(file)) {
 				FileWriter filewriter = new FileWriter(file, true);
 
@@ -26,7 +28,7 @@ public class TextFileWrite {
 	}
 
 	private boolean checkBeforeWritefile(File file) {
-		
+
 		if (file.exists()) {
 			if (file.isFile() && file.canWrite()) {
 				return true;
@@ -34,8 +36,33 @@ public class TextFileWrite {
 		}
 		return false;
 	}
-	
-	public void titleWrite() {
+
+	public void titleWrite(Date date, String fileName) {
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日(E)");
+			File file = new File("C:\\hsakuchi\\work\\ProgressLog\\src\\main\\resources\\templates\\" + fileName + ".txt");
+			
+			if ((checkBeforeWritefile(file)) && (checkFileEmpty(file))) {
+				FileWriter filewriter = new FileWriter(file, true);
+
+				filewriter.write(sdf.format(date) + "\n");
+
+				filewriter.close();
+			} else {
+				System.out.println("すでにタイトルがあります");
+			}
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+
+	private boolean checkFileEmpty(File file) {
+
+		if (file.length() == 0L) {
+            return true;
+        }else {
+        	return false;
+        }
 		
 	}
 
